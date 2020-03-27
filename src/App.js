414f4ddbf02c24af082f928dd608a3b8febb24c9
MrVictor42 <guin-hu@hotmail.com>
2020-03-27 20:15:28 -0300

    Prototype of login in app.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,66 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { connect } from 'react-redux';
 import BaseRouter from './routes';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 import 'antd/dist/antd.css';
+import './css/App.css';
 
 import Layout from './components/layout/Layout';
 
+firebase.initializeApp({
+	apiKey: 'AIzaSyDNAHuQlA_-fCIAKtm469tp56nZYq-jVpw',
+	authDomain: 'grata-auth.firebaseapp.com'
+});
+
 class App extends Component {
+
+	state = { isSignedIn : false }
+	uiConfig = {
+		signInFlow: "popup",
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID
+		],
+		callbacks: {
+		  	signInSuccess: () => false
+		}
+	}
+
+	componentDidMount = () => {
+		firebase.auth().onAuthStateChanged(user => {
+			this.setState({ isSignedIn: !!user })
+		});
+	}
 
 	render() {
 		return (
-			<Router>
-				<Layout {...this.props}>
-					<BaseRouter />
-				</Layout>
-			</Router>
+			<div className = 'App'>
+				{ this.state.isSignedIn ? ( 
+					<span>
+						<div> Signed In! </div>
+						<button onClick = { ()=> firebase.auth().signOut() }> Signout </button>
+						<h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+						<h1>Welcome {firebase.auth().currentUser.email}</h1>
+						<img
+						alt="profile picture"
+						src={firebase.auth().currentUser.photoURL}
+						/>
+					</span>
+				) : (
+					<StyledFirebaseAuth
+						uiConfig = { this.uiConfig }
+						firebaseAuth = { firebase.auth() }
+					/>
+				)}
+			</div>
+			// <Router>
+			// 	<Layout {...this.props}>
+			// 		<BaseRouter />
+			// 	</Layout>
+			// </Router>
 		);
 	}
 }
