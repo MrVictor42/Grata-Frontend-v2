6dd309252ae1a95b5aefae54259c61a299bad953
MrVictor42 <guin-hu@hotmail.com>
2020-04-25 01:04:29 -0300

    fixed footer and started a new form to show information suer

--- src/components/user/UserList.js
+++ src/components/user/UserList.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Input, Button } from 'antd';
-import Highlighter from 'react-highlight-words';
-import { SearchOutlined } from '@ant-design/icons';
+import { Drawer, List, Avatar, Divider, Col, Row } from 'antd';
 
 import { getUsers, getUserToken } from '../../store/user';
 import { getImage } from '../../store/images';
@@ -10,13 +8,57 @@ import { typeUser } from '../../services/userService';
 import UserInfo from './UserInfo';
 import DefaultUser from '../../img/default_user.png';
 
+const pStyle = {
+    fontSize: 16,
+    lineHeight: '24px',
+    display: 'block',
+    marginBottom: 16,
+  };
+
+  const DescriptionItem = ({ title, content }) => (
+    <div
+      className="site-description-item-profile-wrapper"
+      style={{
+        fontSize: 14,
+        lineHeight: '22px',
+        marginBottom: 7,
+      }}
+    >
+      <p
+        className="site-description-item-profile-p"
+        style={{
+          marginRight: 8,
+          display: 'inline-block',
+        }}
+      >
+        {title}:
+      </p>
+      {content}
+    </div>
+  );
+
 class UserList extends Component {
 
-    state = {
-        searchText: '',
-        searchedColumn: '',
-        users: [],
-    };
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            users: [],
+            visible: false
+        }
+    }
+    
+    showDrawer = () => {
+        this.setState({
+          visible: true,
+        });
+      };
+
+      onClose = () => {
+        this.setState({
+          visible: false,
+        });
+      };
 
     async componentDidMount() {
         const token = getUserToken();
@@ -41,139 +83,148 @@ class UserList extends Component {
                 is_administrator: users[aux].is_administrator
             });
         }
-
         this.setState({ users: final_users.users });
     }
 
-    getColumnSearchProps = dataIndex => ({
-        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-            <div style = {{ padding: 8 }}>
-                <Input
-                    ref = { node => { this.searchInput = node }}
-                    placeholder = {`Procure Por ${ dataIndex }`}
-                    value = { selectedKeys[0] }
-                    onChange = { e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-                    onPressEnter = {() => this.handleSearch(selectedKeys, confirm, dataIndex)}
-                    style = {{ width: 188, marginBottom: 8, display: 'block' }}
-                />
-                <Button
-                    type = 'primary'
-                    onClick = {() => this.handleSearch(selectedKeys, confirm, dataIndex)}
-                    icon = { <SearchOutlined /> }
-                    size = 'small'
-                    style = {{ width: 90, marginRight: 8 }}
-                >
-                    Procurar
-                </Button>
-                <Button 
-                    onClick = { () => this.handleReset(clearFilters) } 
-                    size = 'small' 
-                    style = {{ width: 90 }}
-                >
-                    Reiniciar
-                </Button>
-            </div>
-        ),
-        filterIcon: filtered => <SearchOutlined style = {{ color: filtered ? 'red' : undefined }} />,
-        onFilter: (value, record) =>
-            record[dataIndex]
-            .toString()
-            .toLowerCase()
-            .includes(value.toLowerCase()),
-        onFilterDropdownVisibleChange: visible => {
-            if (visible) {
-                setTimeout(() => this.searchInput.select());
-            }
-        },
-        render: text =>
-            this.state.searchedColumn === dataIndex ? (
-                <Highlighter
-                    highlightStyle = {{ backgroundColor: '#ffc069', padding: 0 }}
-                    searchWords = {[ this.state.searchText] }
-                    autoEscape
-                    textToHighlight = { text.toString() }
-                />
-            ) : ( text )
-        });
-    
-        handleSearch = (selectedKeys, confirm, dataIndex) => {
-            confirm();
-            this.setState({
-                searchText: selectedKeys[0],
-                searchedColumn: dataIndex,
+    render() {
+        let data = { users: [] };
+        let typePermission = null;
+
+        for(let aux = 0; aux < this.state.users.length; aux ++) {
+            typePermission = typeUser(this.state.users[aux].is_administrator);
+            data.users.push({
+                key: this.state.users[aux].id,
+                name: this.state.users[aux].name,
+                email: this.state.users[aux].email,
+                ramal: this.state.users[aux].ramal,
+                image: this.state.users[aux].image,
+                description: 'Não Feito',
+                sector: 'Não Feito',
+                permission: typePermission
             });
-        };
-    
-        handleReset = clearFilters => {
-            clearFilters();
-            this.setState({ searchText: '' });
-        };
-
-        render() {
-            const columns = [{
-                title: 'Nome',
-                dataIndex: 'Nome',
-                key: 'Nome',
-                width: '20%',
-                ...this.getColumnSearchProps('Nome'),
-                },{
-                    title: 'Email',
-                    dataIndex: 'Email',
-                    key: 'Email',
-                    width: '20%',
-                    ...this.getColumnSearchProps('Email'),
-                },{
-                    title: 'Setor',
-                    dataIndex: 'Setor',
-                    key: 'Setor',
-                    width: '20%',
-                    ...this.getColumnSearchProps('Setor'),
-                },
-                {
-                    title: 'Ramal',
-                    dataIndex: 'Ramal',
-                    key: 'Ramal',
-                    width: '10%',
-                    ...this.getColumnSearchProps('Ramal'),
-                },
-                {
-                    title: 'Permissão',
-                    dataIndex: 'Permissao',
-                    key: 'Permissao',
-                    width: '10%',
-                    ...this.getColumnSearchProps('Permissao'),
-                },
-                {
-                    title: 'Opção',
-                    key: 'option',
-                    width: '10%',
-                    render: (record) => (
-                        <UserInfo user = { record } />
-                    ),
-                }
-            ];
-            let data = { users: [] };
-            let typePermission = null;
-
-            for(let aux = 0; aux < this.state.users.length; aux ++) {
-                typePermission = typeUser(this.state.users[aux].is_administrator);
-                data.users.push({
-                    key: this.state.users[aux].id,
-                    Nome: this.state.users[aux].name,
-                    Email: this.state.users[aux].email,
-                    Ramal: this.state.users[aux].ramal,
-                    image: this.state.users[aux].image,
-                    Setor: 'Não Feito',
-                    Permissao: typePermission
-                });
-            }
+        }
         return (
-            <Table 
-                columns = { columns } className = 'userList' 
-                dataSource = { data.users } pagination = {{ defaultPageSize: 5 }} 
-            />
+            <div>
+                <List
+                    dataSource = { data.users } pagination = {{ defaultPageSize: 4 }} 
+                    bordered className = 'userList'
+                    renderItem = { user => (
+                        <List.Item
+                            key = { user.key }
+                            actions = {[
+                                <a onClick = { this.showDrawer } key = {`a-${ user.key }`}>
+                                    Visualizar Perfil
+                                </a>,
+                            ]}
+                        >
+                        <List.Item.Meta
+                            avatar = { <Avatar src = { user.image } /> }
+                            title = { user.name}
+                            description = { user.description }
+                        />
+                        </List.Item>
+                    )}
+                />
+            <Drawer
+              width={640}
+              placement="right"
+              closable={false}
+              onClose={this.onClose}
+              visible={this.state.visible}
+            >
+              <p className="site-description-item-profile-p" style={{ ...pStyle, marginBottom: 24 }}>
+                User Profile
+              </p>
+              <p className="site-description-item-profile-p" style={pStyle}>
+                Personal
+              </p>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="Full Name" content="Lily" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Account" content="AntDesign@example.com" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="City" content="HangZhou" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Country" content="China🇨🇳" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="Birthday" content="February 2,1900" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Website" content="-" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <DescriptionItem
+                    title="Message"
+                    content="Make things as simple as possible but no simpler."
+                  />
+                </Col>
+              </Row>
+              <Divider />
+              <p className="site-description-item-profile-p" style={pStyle}>
+                Company
+              </p>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="Position" content="Programmer" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Responsibilities" content="Coding" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="Department" content="XTech" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Supervisor" content={<a>Lin</a>} />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <DescriptionItem
+                    title="Skills"
+                    content="C / C + +, data structures, software engineering, operating systems, computer networks, databases, compiler theory, computer architecture, Microcomputer Principle and Interface Technology, Computer English, Java, ASP, etc."
+                  />
+                </Col>
+              </Row>
+              <Divider />
+              <p className="site-description-item-profile-p" style={pStyle}>
+                Contacts
+              </p>
+              <Row>
+                <Col span={12}>
+                  <DescriptionItem title="Email" content="AntDesign@example.com" />
+                </Col>
+                <Col span={12}>
+                  <DescriptionItem title="Phone Number" content="+86 181 0000 0000" />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <DescriptionItem
+                    title="Github"
+                    content={
+                      <a href="http://github.com/ant-design/ant-design/">
+                        github.com/ant-design/ant-design/
+                      </a>
+                    }
+                  />
+                </Col>
+              </Row>
+            </Drawer>
+          </div>
         );
-    }
+      }    
 }
-
 export default UserList;
