88093708d9cc9dd9cfbee6dbbd2beca73413f0cd
MrVictor42 <guin-hu@hotmail.com>
2020-05-20 15:24:04 -0300

    Started items of meeting

--- src/components/forms/meeting/items/Items.js
+++ src/components/forms/meeting/items/Items.js
@@ -1,13 +1,97 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
-import { OrderedListOutlined } from '@ant-design/icons';
+import { Button, Drawer, Tabs, Form, notification } from 'antd';
+import { 
+    OrderedListOutlined, BookOutlined, AccountBookOutlined, SaveOutlined, StopOutlined 
+} from '@ant-design/icons';
+
+import MeetingAgenda from './MeetingAgenda';
+import RulesMeeting from './RulesMeeting';
+
+const { TabPane } = Tabs;
 
 class Items extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            visible: false
+        }
+
+        this.showDrawer = this.showDrawer.bind(this);
+        this.onClose = this.onClose.bind(this);
+    }
+
+    showDrawer = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+    
+    onClose = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    async handleSubmit(values) {
+
+    }
+
     render() {
+        const FormItems = () => {
+            const [form] = Form.useForm();
+            return(
+                <Drawer
+                    title = { `Itens Para Composição da Reunião: ${ this.props.meeting.title }` } 
+                    onClose = { this.onClose } width = { '50%' }
+                    visible = { this.state.visible } 
+                    footer = { 
+                        <div style = {{ textAlign: 'center' }}>
+                            <Button onClick = { this.onClose } style = {{ marginRight: 8 }}>
+                                <StopOutlined /> Cancelar
+                            </Button>
+                            <Button onClose = { this.onClose } type = 'primary' 
+                                onClick = { () => {
+                                    form.validateFields().then(values => {
+                                        form.resetFields();
+                                        this.handleSubmit(values);
+                                    }).catch(info => {
+                                        console.log('Validate Failed:', info);
+                                    });
+                                }}>
+                                <SaveOutlined /> Cadastrar Itens
+                            </Button>
+                        </div>
+                    }
+                >
+                    <Form hideRequiredMark layout = 'vertical'>
+                        <Tabs defaultActiveKey = '1'>
+                            <TabPane
+                                tab = { <span><BookOutlined /> Pautas da Reunião </span> }
+                                key = '1'
+                            >
+                                <MeetingAgenda form = { form } />
+                            </TabPane>
+
+                            <TabPane
+                                tab = { <span><AccountBookOutlined /> Regras da Reunião </span> }   
+                                key = '2'
+                            >
+                                <RulesMeeting form = { form } />
+                            </TabPane>
+                        </Tabs>
+                    </Form>
+                </Drawer>
+            );
+        }
         return (
-            <Button>
-                <OrderedListOutlined /> <b> Items </b>
-            </Button>
+            <span>
+                <Button onClick = { this.showDrawer }>
+                    <OrderedListOutlined /> <b> Items </b>
+                </Button>
+                <FormItems />
+            </span>
         );
     }
 }
---
+++ src/components/forms/meeting/items/MeetingAgenda.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Form, Input, Button } from 'antd';
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons';
+
+class MeetingAgenda extends Component {
+
+    render() {
+        return (
+            <Form form = { this.props.form }>
+                <h2 align = 'center'> Pautas da Reunião </h2>
+                <Form.List name = 'agenda'>
+                    {(fields, { add, remove }) => {
+                        return (
+                            <div>
+                                { fields.map((field, index) => (
+                                    <Form.Item
+                                        { ...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel) }
+                                        label = { index === 0 ? 'Tópicos' : ''}
+                                        required = { true }
+                                        key = { field.key }
+                                        className = 'formList'
+                                    >
+                                        <Form.Item
+                                            { ...field }
+                                            validateTrigger = {['onChange', 'onBlur']}
+                                            rules = {[{
+                                                required: true,
+                                                whitespace: true,
+                                                message: 'Por Favor, Adicione Pelo Menos um Tópico ' + 
+                                                        'a Reunião, ou Exclua Este Campo!' 
+                                            }]}
+                                            noStyle
+                                        >
+                                            <Input 
+                                                placeholder = 'Tópico da Pauta' 
+                                                style = {{ width: '60%' }} 
+                                            />
+                                        </Form.Item>
+                                        { fields.length > 1 ? (
+                                            <MinusCircleOutlined
+                                                className = 'dynamic-delete-button'
+                                                style = {{ margin: '0 8px' }}
+                                                onClick = {() => {
+                                                    remove(field.name);
+                                                }}
+                                            />
+                                        ) : null }
+                                    </Form.Item>
+                                ))}
+                                <Form.Item>
+                                    <Button
+                                        type = 'dashed' onClick = {() => { add() }}
+                                        style = {{ width: '60%', marginLeft: 120 }}
+                                    >
+                                        <PlusOutlined /> Adicionar Tópicos
+                                    </Button>
+                                </Form.Item>
+                            </div>
+                        );
+                    }}
+                </Form.List>
+            </Form>
+        );
+    }
+}
+
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+    },
+};
+const formItemLayoutWithOutLabel = {
+    wrapperCol: {
+        xs: { span: 20, offset: 0 },
+        sm: { span: 20, offset: 0 },
+    },
+};
+
+export default MeetingAgenda;
